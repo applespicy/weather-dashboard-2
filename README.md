d66f9b296b9d8d74e516acdf8e2b7dbd110f0052
applespicy <d.oshinowo@yahoo.com>
2022-06-18 23:12:22 -0400

    weather dashboard

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# weather-dashboard-2
+# weather-dashboard
+
+## PROJECT DESCRIPTION
+
+This application helps users to get the weather situation in a particular city and the forcast for the next seven days
+
+Upon loading the application shows the humdity pressure and windspeed for the current location
+
+it aslo gives a current day forcast of the temperature during the day and the temperature during the night and aslo for the next seven days
+
+
+## USER STORY
+
+AS a user
+I Want to know the Weather forcast for my location
+
+## ACCEPTANCE CRITERIA 
+
+WHEN i open the App, i need to see the humdity, pressure and windspeed of my location
+THEN I am presented with a forcast for the next 7 days for the temp for both day and night
+
+
+
+## Technologies used:
+
+HTML
+CSS
+javascript
+Moment
+OPen weather API
--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const currentWeatherItemsE1= document.getElementById('current-weather-items');
 const timezone =document.getElementById('time-zone');
 const weatherForcastE1 =document.getElementById('weather-forcast');
 const currentTempE1= document.getElementById('current-temp');
+let humdityE1=document.getElementById('hum');
 
 const days=['sunday','Monday','Tuesday','Wednesday', 'Thursday','Friday','Saturday'];
 const months=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Sep','Oct','Nov','Dec'];
@@ -30,10 +31,53 @@ function getWeatherData(){
         let long = success.coords.longitude
         let lan =success.coords.latitude
 
-        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lan}&lon=${long}&exclude=hourly&minutely&appid=${API_KEY}`)
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lan}&lon=${long}&exclude=hourly&minutely&units=metrics&appid=${API_KEY}`)
         .then(res=>res.json()).then(data=>{
             console.log(data)
+            showWeatherData(data);
         })
+       
     })
 }
 getWeatherData()
+function showWeatherData(data){
+    let {humidity, pressure, wind_speed} = data.current;
+    timezone.innerHTML=data.timezone,
+    
+    currentWeatherItemsE1.innerHTML =
+                `<div class="weather-items">
+                    <div>humidity</div>
+                    <div>${humidity}</div>
+                </div>
+                <div class="weather-items">
+                    <div>pressure</div>
+                    <div>${pressure}</div>
+                </div>
+                <div class="weather-items">
+                    <div>windspeed</div>
+                    <div>${wind_speed}</div>
+                </div>`
+
+        let otherDayForcast=''
+        data.daily.forEach((day, idx) => {
+            if (idx ==0){
+                currentTempE1.innerHTMl= `<div class="weather-forcast-items">
+                <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+            <div class="day">${moment(day.dt*1000).format(`ddd`)}</div>
+            <div class="temp">${day.temp.night}&#176;c</div>
+            <div class="temp">${day.temp.day}&#176;c</div>
+            </div>`
+            }else{
+                otherDayForcast +=
+               `<div class="weather-forcast-items">
+               <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+           <div class="day">${moment(day.dt*1000).format(`ddd`)}</div>
+           <div class="temp">Night ${day.temp.night}&#176;c</div>
+           <div class="temp">Day ${day.temp.day}&#176;c</div>
+           </div>` 
+                
+            }
+        } )   
+      
+        weatherForcastE1.innerHTML =otherDayForcast;
+    }
